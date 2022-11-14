42f4a4b2c780256f51d4b5abbbd41f44e7cfe80f
Ashlynn223 <50315144+Ashlynn223@users.noreply.github.com>
2022-11-13 21:37:43 -0800

    Initial Commit

---
+++ Shared/riseApp.swift
@@ -0,0 +1,17 @@
+//
+//  riseApp.swift
+//  Shared
+//
+//  Created by Ashlynn on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct riseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
