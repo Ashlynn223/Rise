571be3f0d453ba8aa6ee1c2c715f3dada59d9581
Ashlynn223 <50315144+Ashlynn223@users.noreply.github.com>
2022-11-15 16:23:10 -0800

    add basic time picker

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -3,10 +3,12 @@ import AVKit
 
 struct ContentView: View {
     @StateObject private var viewModel = ViewModel()
+    @State private var currentDate = Date()
     var body: some View {
         VStack {
             Text("Hello, World!")
                 .padding()
+            DatePicker("", selection: $currentDate, displayedComponents: .hourAndMinute)
             VideoPlayer(player: AVPlayer(url: viewModel.videoUrl as URL))
         }
     }
